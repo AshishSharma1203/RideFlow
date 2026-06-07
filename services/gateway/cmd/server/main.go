04e2d11b7a6c834998622ada1908da110227d055
Ashish <akrsharma766@gmail.com>
2026-06-07 20:22:05 +0530

    Implemented graceful shutdown

--- services/gateway/cmd/server/main.go
+++ services/gateway/cmd/server/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/labstack/echo/v4"
 
@@ -12,6 +18,14 @@ import (
 func main() {
 
 	e := echo.New()
+
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+
+	defer stop()
 
 	identityClient, err := client.NewIdentityClient()
 	if err != nil {
@@ -29,7 +43,35 @@ func main() {
 
 	log.Println("gateway listening on :8080")
 
-	if err := e.Start(":8080"); err != nil {
-		log.Fatal(err)
+	go func() {
+
+		if err := e.Start(":8080"); err != nil &&
+			err != http.ErrServerClosed {
+
+			log.Fatalf("echo start error: %v", err)
+		}
+
+	}()
+
+	<-ctx.Done()
+
+	log.Println("shutdown signal received")
+	shutdownCtx, cancel := context.WithTimeout(
+		context.Background(),
+		10*time.Second,
+	)
+
+	defer cancel()
+
+	if err := e.Shutdown(shutdownCtx); err != nil {
+		log.Printf("echo shutdown error: %v", err)
 	}
+	if err := identityClient.Conn.Close(); err != nil {
+		log.Printf(
+			"identity grpc close error: %v",
+			err,
+		)
+	}
+
+	log.Println("gateway stopped")
 }
